eb85e859d26dd7d7c967b0a3d76698bf6733c673
thomas.spencer@codurance.com <thomas.spencer@codurance.com>
2020-11-29 17:59:11 +0000

    Add code for largest palindrome problem 4

--- problems1_5.py
+++ problems1_5.py
@@ -77,9 +77,9 @@ def get_factors(number):
             factors.append(number // potentialFactor)
     return factors
 
+
 def is_prime(input):
     return len(get_factors(input)) == 2
-
 
 
 all_factors = get_factors(600_851_475_143)
@@ -89,3 +89,42 @@ for factor in all_factors:
         largest_prime = factor
 
 logging.debug("3. The largest prime factor of 600851475143 is %s", largest_prime)
+
+
+# Problem 4. find the largest palindrome product of 2 n digit numbers
+# 1. multiply 999 * 999
+# 2. work out if number is a palindrome
+# 2. if it is store it in array
+# 3. find largest
+
+
+def is_palindrome(number):
+    reversed = 0
+    temp = number
+    while (temp > 0):
+        last_digit = temp % 10
+        reversed = int(reversed * 10 + last_digit)
+        temp = int(temp // 10)
+    return number == reversed
+
+
+def largest_palindrome(n):
+    return helper(n)
+
+
+def helper(n):
+    largest_number = int(math.pow(10, n) - 1)
+    smallest_number = int(math.pow(10, (n - 1)))
+    largest_palindrome = 0
+    for outer_number in range(largest_number, smallest_number, -1):
+        for inner_number in range(outer_number, smallest_number, -1):
+            number = int(outer_number * inner_number)
+            if is_palindrome(number) and number > largest_palindrome:
+                largest_palindrome = number
+                if inner_number > smallest_number:
+                    smallest_number = inner_number
+                break
+    return largest_palindrome
+
+
+logging.debug(largest_palindrome(4))
