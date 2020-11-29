662e3d7ff3436815de25336b8c0f0c7f6fe72616
thomas.spencer@codurance.com <thomas.spencer@codurance.com>
2020-11-29 15:47:50 +0000

    First problem

---
+++ problems1_5.py
@@ -0,0 +1,53 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+# Problem 1: Find the sum of all the multiples of 3 or 5 below 1000
+# find multiples of 3 or 5 below 1000
+# add the multiples together
+
+def sumMultiples():
+    sum = 0
+    for number in range(1, 1000):
+        if (number % 3 == 0) or (number % 5 == 0):
+            sum += number
+    return sum
+
+
+result = sumMultiples()
+logging.debug("1. Sum of multiples of 3 and 5 below 1000 is %s", result)
+
+
+def sum_multiples_recursion():
+    n = 1000
+    n -= 1
+    return int(3 * sum_numbers(n / 3) + 5 * sum_numbers(n / 5) - 15 * sum_numbers(n / 15))
+
+
+def sum_numbers(n):
+    return n * (n + 1) // 2
+
+logging.debug("1.(b) Sum of multiples of 3 and 5 below 1000 is %s", sum_multiples_recursion())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
